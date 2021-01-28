55bad5af4890460a2f70f83b21865c9b85f9058c
KrisJackson <kristopherrjackson@gmail.com>
2021-01-28 16:55:54 -0600

    Initial commit

---
+++ DrinkUp_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkUp_iOS
+//
+//  Created by Kristopher Jackson on 1/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
