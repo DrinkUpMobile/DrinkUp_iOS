6aa920bad36688fcd1e44849940faa88e230584f
KrisJackson <kristopherrjackson@gmail.com>
2021-02-09 17:07:17 -0600

    Began login UI

---
+++ DrinkUp_iOS/Globals/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  DrinkUp_iOS
+//
+//  Created by Kristopher Jackson on 2/9/21.
+//
+
+import UIKit
+
+class Colors {
+    
+    static let main: UIColor = UIColor(red: 0.2666666667, green: 0.5411764706, blue: 1, alpha: 1)
+    static let grayText: UIColor = UIColor(red: 0.3254901961, green: 0.3254901961, blue: 0.3254901961, alpha: 1)
+    
+}
+
